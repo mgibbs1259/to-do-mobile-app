95f01bf9259550160dbee9809ee95d09d5f7ce7e
Mary <mgibbs1259@gmail.com>
2019-07-20 11:54:53 -0400

    title and background

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-
-export default class HelloWorldApp extends Component {
+import React from 'react';
+import { ImageBackground, Text, View, StyleSheet } from 'react-native';
+export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <ImageBackground
+        source={require('./blue_background.jpeg')}
+        style={{width: '100%', height: '100%'}}>
+        <View style={styles.container}>
+          <Text style={styles.titleText}> Get Shit Done! </Text>
+        </View>
+      </ImageBackground>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titleText: {
+    color: 'white',
+    fontSize: 36,
+    fontWeight: 'bold'
+  },
+});
