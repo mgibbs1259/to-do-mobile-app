f3d7dd7488b4d7d3b091d0453e689dbee3794a0c
Mary <mgibbs1259@gmail.com>
2019-08-14 18:01:40 -0400

    working home screen button

--- App.js
+++ App.js
@@ -1,47 +1,10 @@
-import React from 'react';
-import {
-  Text,
-  View,
-  StatusBar,
-  StyleSheet } from 'react-native';
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar
-          barStyle='light-content'
-          backgroundColor='#87CEEB'
-        />
-        <Text style={styles.greeting}>
-          Get Shit Done!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, tap the screen.
-        </Text>
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#87CEEB',
-  },
-  greeting: {
-    color: 'white',
-    fontFamily: 'System',
-    fontSize: 36,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    color: 'white',
-    fontFamily: 'System',
-    fontSize: 26,
-    textAlign: 'center',
-    marginBottom: 5,
-  },
+import React, { Component } from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './HomeScreen';
+import ToDoScreen from './ToDoScreen';
+const MainNavigator = createStackNavigator({
+  HomeScreen: {screen: HomeScreen},
+  ToDoScreen: {screen: ToDoScreen},
 });
+const App = createAppContainer(MainNavigator);
+export default App;
